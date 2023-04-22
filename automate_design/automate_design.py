31e008c6da773457635af4bafdb62af2f345ae0b
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-04-22 11:15:56 -0400

    feat(TORCC-5): working prototype

--- automate_design/automate_design.py
+++ automate_design/automate_design.py
@@ -8,14 +8,15 @@ design_image = Image.open(design_path).convert("RGBA")
 clothing_image = Image.open(clothing_path)
 
 # Resize the design image to fit on the clothing image
-design_size = (int(clothing_image.size[0] / 2), int(clothing_image.size[1] / 2))
-design_image = design_image.resize(design_size)
+max_width = int(clothing_image.size[0] / 2)
+max_height = int(clothing_image.size[1] / 2)
+design_image.thumbnail((max_width, max_height), Image.LANCZOS)
 
 # Paste the clothing image onto the canvas
 final_image = clothing_image.copy()
 
 # Paste the design image onto the clothing image
-design_position = (int((clothing_image.size[0] - design_size[0]) / 2), int((clothing_image.size[1] - design_size[1]) / 2))
+design_position = (int((clothing_image.size[0] - design_image.size[0]) / 2), int((clothing_image.size[1] - design_image.size[1]) / 2))
 final_image.paste(design_image, design_position, design_image)
 
 # Save the final image in PNG format
