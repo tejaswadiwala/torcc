2201ea81652752b33659f793180d9348bfbab1e8
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-04-23 22:07:39 -0400

    chore(TORCC-5): add logger (#9)

--- automate_design/automate_design.py
+++ automate_design/automate_design.py
@@ -9,25 +9,35 @@ design_image = Image.open(design_path).convert("RGBA")
 clothing_dir = "automate_design/clothing_images/"
 export_dir = "automate_design/exports/"
 
-# Loop over all the images in the clothing directory
-for filename in os.listdir(clothing_dir):
-    if filename.endswith(".png"):
-        # Set up the clothing image
-        clothing_path = os.path.join(clothing_dir, filename)
-        clothing_image = Image.open(clothing_path).convert("RGBA")
+def automate_design():
+    try: 
+        print('Automate Design: Starting now.')
+        # Loop over all the images in the clothing directory
+        for filename in os.listdir(clothing_dir):
+            if filename.endswith(".png"):
+                print(f'Automate Design: Starting to add design to {filename}.')
+                # Set up the clothing image
+                clothing_path = os.path.join(clothing_dir, filename)
+                clothing_image = Image.open(clothing_path).convert("RGBA")
 
-        # Resize the design image to fit on the clothing image
-        max_width = int(clothing_image.size[0] / 2)
-        max_height = int(clothing_image.size[1] / 2)
-        design_image.thumbnail((max_width, max_height), Image.LANCZOS)
+                # Resize the design image to fit on the clothing image
+                max_width = int(clothing_image.size[0] / 2)
+                max_height = int(clothing_image.size[1] / 2)
+                design_image.thumbnail((max_width, max_height), Image.LANCZOS)
 
-        # Paste the clothing image onto the canvas
-        final_image = clothing_image.copy()
+                # Paste the clothing image onto the canvas
+                final_image = clothing_image.copy()
 
-        # Paste the design image onto the clothing image
-        design_position = (int((clothing_image.size[0] - design_image.size[0]) / 2), int((clothing_image.size[1] - design_image.size[1]) / 2))
-        final_image.paste(design_image, design_position, design_image)
+                # Paste the design image onto the clothing image
+                design_position = (int((clothing_image.size[0] - design_image.size[0]) / 2), int((clothing_image.size[1] - design_image.size[1]) / 2))
+                final_image.paste(design_image, design_position, design_image)
 
-        # Save the final image in PNG format
-        export_path = os.path.join(export_dir, f"{filename}")
-        final_image.save(export_path)
+                # Save the final image in PNG format
+                export_path = os.path.join(export_dir, f"{filename}")
+                final_image.save(export_path)
+                print(f'Automate Design: Added design to {export_path}.')
+        print('Automate Design: Successfully completed execution.')
+    except Exception as e:
+        print(f'Automate Design: Error occurred - {e}.')
+
+automate_design()
