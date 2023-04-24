70f7c8a08805aa8d9fbbf39198062eb46631dfba
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-04-24 17:00:44 -0400

    chore(TORCC-5,6): add oversized (#11)

---
+++ automate_design/Product.py
@@ -0,0 +1,8 @@
+class Product:
+    class ClassicTee:
+        def __init__(self): 
+            self.dir_name = 'classic_tees'
+
+    class OversizedTshirt:
+        def __init__(self): 
+            self.dir_name = 'oversized_tees'
