65132dff24cc669481866611cd22a51191466e0a
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-04-23 21:54:20 -0400

    chore(TORCC-5): add iteration over clothing images

--- automate_design/automate_design.py
+++ automate_design/automate_design.py
@@ -1,23 +1,33 @@
+import os
 from PIL import Image
 
-# Set up the design and clothing images
+# Set up the design image
 design_path = "automate_design/designs/design_1.png"
-clothing_path = "automate_design/clothing_images/navy_classic_tee.png"
-
 design_image = Image.open(design_path).convert("RGBA")
-clothing_image = Image.open(clothing_path)
 
-# Resize the design image to fit on the clothing image
-max_width = int(clothing_image.size[0] / 2)
-max_height = int(clothing_image.size[1] / 2)
-design_image.thumbnail((max_width, max_height), Image.LANCZOS)
+# Set up the directory paths
+clothing_dir = "automate_design/clothing_images/"
+export_dir = "automate_design/exports/"
 
-# Paste the clothing image onto the canvas
-final_image = clothing_image.copy()
+# Loop over all the images in the clothing directory
+for filename in os.listdir(clothing_dir):
+    if filename.endswith(".png"):
+        # Set up the clothing image
+        clothing_path = os.path.join(clothing_dir, filename)
+        clothing_image = Image.open(clothing_path).convert("RGBA")
 
-# Paste the design image onto the clothing image
-design_position = (int((clothing_image.size[0] - design_image.size[0]) / 2), int((clothing_image.size[1] - design_image.size[1]) / 2))
-final_image.paste(design_image, design_position, design_image)
+        # Resize the design image to fit on the clothing image
+        max_width = int(clothing_image.size[0] / 2)
+        max_height = int(clothing_image.size[1] / 2)
+        design_image.thumbnail((max_width, max_height), Image.LANCZOS)
 
-# Save the final image in PNG format
-final_image.save("automate_design/exports/mockup.png")
+        # Paste the clothing image onto the canvas
+        final_image = clothing_image.copy()
+
+        # Paste the design image onto the clothing image
+        design_position = (int((clothing_image.size[0] - design_image.size[0]) / 2), int((clothing_image.size[1] - design_image.size[1]) / 2))
+        final_image.paste(design_image, design_position, design_image)
+
+        # Save the final image in PNG format
+        export_path = os.path.join(export_dir, f"{filename}")
+        final_image.save(export_path)
