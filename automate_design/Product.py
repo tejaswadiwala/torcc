99832331a5681efb30c226e39baf261d5b37d168
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-05-31 22:57:35 -0400

    feat(TORCC-7): add tote bags (#13)

--- automate_design/Product.py
+++ automate_design/Product.py
@@ -6,3 +6,7 @@ class Product:
     class OversizedTshirt:
         def __init__(self): 
             self.dir_name = 'oversized_tees'
+    
+    class ToteBags:
+        def __init__(self):
+            self.dir_name = 'tote_bags'
